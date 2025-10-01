2af531605536ebdaf94a1464d0c41349f409f4c7
Ricardo Reis <ricardo@plckr.dev>
2025-10-01 23:10:14 +0100

    devtools vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { cloudflare } from '@cloudflare/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
@@ -7,6 +8,9 @@ import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const config = defineConfig({
   plugins: [
+    devtools({
+      enhancedLogs: { enabled: true }
+    }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json']
