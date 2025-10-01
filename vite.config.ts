6fb98f7e8dfc04950fb1796513d214d009bd69ed
Ricardo Reis <ricardo@plckr.dev>
2025-10-01 09:18:48 +0100

    add cloudflare support

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { cloudflare } from '@cloudflare/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
@@ -10,6 +11,7 @@ const config = defineConfig({
     viteTsConfigPaths({
       projects: ['./tsconfig.json']
     }),
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     tanstackStart(),
     viteReact()
