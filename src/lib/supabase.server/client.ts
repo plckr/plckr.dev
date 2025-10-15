fdc7d88b748004a0c844e847e4984713a95f7eff
Ricardo Reis <ricardo@plckr.dev>
2025-10-15 19:11:08 +0100

    add supabase types

--- src/lib/supabase.server/client.ts
+++ src/lib/supabase.server/client.ts
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
+import { Database } from './types';
+
 const projectId = import.meta.env.VITE_SUPABASE_PROJECT_ID;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 const supabaseUrl = `https://${projectId}.supabase.co`;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
