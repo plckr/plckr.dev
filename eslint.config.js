e187b3550a117822bc8bea1185a24e9f134d0d26
Ricardo Reis <ricardo@plckr.dev>
2025-11-14 23:09:37 +0000

    prettier write

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 //  @ts-check
 import { tanstackConfig } from '@tanstack/eslint-config';
+import storybook from 'eslint-plugin-storybook';
 
-export default [...tanstackConfig, ...storybook.configs["flat/recommended"]];
+export default [...tanstackConfig, ...storybook.configs['flat/recommended']];
